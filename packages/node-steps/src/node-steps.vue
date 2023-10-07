35817ea0daa4c6f1fd98dc231d35231bf8a84cba
guoqin <1178562936@qq.com>
2023-10-07 17:01:12 +0800

    fix:css测试:deep、vue中添加css、引入src中的样式文件

--- packages/node-steps/src/node-steps.vue
+++ packages/node-steps/src/node-steps.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-steps :active="2" align-center>
+  <el-steps :active="2" align-center class="steps-wrap">
     <el-step title="Step 1" description="Some description" />
     <el-step title="Step 2" description="Some description" />
     <el-step title="Step 3" description="Some description" />
@@ -7,8 +7,18 @@
   </el-steps>
 </template>
 <script lang="ts" setup>
+import '../../../src/assets/index.css'
+import './index.scss'
 const COMPONENT_NAME = 'SzywNodeSteps'
 defineOptions({
   name: COMPONENT_NAME
 })
 </script>
+<style scoped>
+:deep(.is-finish){
+  color: #06a71d!important;
+}
+.steps-wrap {
+  font-size: 4px;
+}
+</style>
