a94f972a455b72360f59868178ef3281a60d85f6
guoqin <1178562936@qq.com>
2023-10-18 14:38:37 +0800

    fix: 修改css

--- vite-lib.config.ts
+++ vite-lib.config.ts
@@ -35,13 +35,13 @@ export default defineConfig({
       // 确保外部化处理那些你不想打包进库的依赖
       external: ['vue'],
       output: {
-        format: 'umd',
+        // format: 'umd',
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
           vue: 'Vue',
           'element-plus': 'ElementPlus',
         },
-        // assetFileNames: `szyw-bc-vue.style.css`,
+        assetFileNames: `assets/styles/szyw-bc-vue.style.css`, //决定打包后静态文件的路径及名称
       },
     },
   },
