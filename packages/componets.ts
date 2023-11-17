45bceeb4de9a2c62d24996dd825ccb257ff01f18
guoqin <1178562936@qq.com>
2023-11-17 14:22:42 +0800

    fix: 修改批量引入

--- packages/componets.ts
+++ packages/componets.ts
@@ -2,7 +2,8 @@ import { SzywProjectList } from '../packages/project-list'
 import { SzywNodeSteps } from '../packages/node-steps'
 import { SzywTreeTransfer } from '../packages/tree-transfer'
 // 用于批量引入的遍历
-export default [SzywTreeTransfer, SzywProjectList, SzywNodeSteps]
+const allComponents = [SzywTreeTransfer, SzywProjectList, SzywNodeSteps]
+export default allComponents
 export {
   SzywProjectList,
   SzywTreeTransfer,
